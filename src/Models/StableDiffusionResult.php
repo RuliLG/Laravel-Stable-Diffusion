9988845addde5cb6ed3c97aaf64b782508d302db
Raúl López <hi@raullg.com>
2022-09-08 21:27:13 +0200

    feat: add new attributes

--- src/Models/StableDiffusionResult.php
+++ src/Models/StableDiffusionResult.php
@@ -2,6 +2,7 @@
 
 namespace RuliLG\StableDiffusion\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,5 +20,33 @@ class StableDiffusionResult extends Model
     public function scopeUnfinished($query)
     {
         return $query->whereIn('status', ['starting', 'processing']);
+    }
+
+    public function isSuccessful(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->status === 'succeeded',
+        );
+    }
+
+    public function isStarting(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->status === 'starting',
+        );
+    }
+
+    public function isProcessing(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->status === 'processing',
+        );
+    }
+
+    public function isFailed(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->status === 'failed',
+        );
     }
 }
